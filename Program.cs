1e4e6f04d8d986cab8d4a2a831ed394b199fa5c1
yameste <yameste@yahoo.co.uk>
2020-02-27 09:33:48 +0000

    added modulo to show remainder

--- Program.cs
+++ Program.cs
@@ -53,6 +53,13 @@ namespace numbersincsharp
             WorkingWithIntegers();
             OrderPrecedence();
             
+            int a = 7;
+            int b = 4;
+            int c = 3;
+            int d = (a + b) / c;
+            int e = (a + b) % c;
+            Console.WriteLine($"quotient: {d}");
+            Console.WriteLine($"remainder: {e}");
                     
             
         }
