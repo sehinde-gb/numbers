e7c8027e9a31f55f9be7b3f95114e070667a06ae
yameste <yameste@yahoo.co.uk>
2020-02-27 08:49:49 +0000

    second commit added addition subtraction multiplication and division methods

--- Program.cs
+++ Program.cs
@@ -8,7 +8,21 @@ namespace numbersincsharp
         {
             int a = 18;
             int b = 6;
+
+            // addition
             int c = a + b;
+            Console.WriteLine(c);
+
+            // subtraction
+            c = a - b;
+            Console.WriteLine(c);
+
+            // multiplication
+            c = a * b;
+            Console.WriteLine(c);
+
+            // division
+            c = a / b;
             Console.WriteLine(c);
         }
     }
