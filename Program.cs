3de43c317fd4e1602aed6891494c19054b4aa8c3
yameste <yameste@yahoo.co.uk>
2020-02-27 09:29:12 +0000

    created new method order precdence

--- Program.cs
+++ Program.cs
@@ -26,15 +26,35 @@ namespace numbersincsharp
             Console.WriteLine(c);
         }
 
-        static void Main(string[] args)
+        static void OrderPrecedence()
         {
-            //WorkingWithIntegers();
             int a = 5;
             int b = 4;
             int c = 2;
-            int d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
-                    
+            int d = a + b * c;
             Console.WriteLine(d);
+
+            d = (a + b) * c;
+            Console.WriteLine(d);
+
+            d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+            Console.WriteLine(d);
+
+            int e = 7;
+            int f = 4;
+            int g = 3;
+            int h = (e  + f) / g;
+            Console.WriteLine(h);
+
+        }
+
+        static void Main(string[] args)
+        {
+            WorkingWithIntegers();
+            OrderPrecedence();
+            
+                    
+            
         }
     }
 }
