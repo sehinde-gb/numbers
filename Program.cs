04c61582d8833235b576e78abf6bddc1d7cf9abf
yameste <yameste@yahoo.co.uk>
2020-02-27 09:01:01 +0000

    multiplication comes before addition in numerical methods

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace numbersincsharp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void WorkingWithIntegers()
         {
             int a = 18;
             int b = 6;
@@ -24,6 +24,16 @@ namespace numbersincsharp
             // division
             c = a / b;
             Console.WriteLine(c);
+        }
+
+        static void Main(string[] args)
+        {
+            //WorkingWithIntegers();
+            int a = 5;
+            int b = 4;
+            int c = 2;
+            int d = a + b * c;
+            Console.WriteLine(d);
         }
     }
 }
