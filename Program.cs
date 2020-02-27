e8bb104cc702031c1f856897bf43f07cef66e0d4
yameste <yameste@yahoo.co.uk>
2020-02-27 09:08:09 +0000

    more complex operators

--- Program.cs
+++ Program.cs
@@ -32,7 +32,8 @@ namespace numbersincsharp
             int a = 5;
             int b = 4;
             int c = 2;
-            int d = (a + b) * c;
+            int d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+                    
             Console.WriteLine(d);
         }
     }
