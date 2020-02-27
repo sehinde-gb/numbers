281c69f2b95be71b1bc380bb2cc100e003295215
yameste <yameste@yahoo.co.uk>
2020-02-27 09:37:46 +0000

    tested limits of integer

--- Program.cs
+++ Program.cs
@@ -48,19 +48,34 @@ namespace numbersincsharp
 
         }
 
-        static void Main(string[] args)
+
+        static void TestLimits() 
         {
-            WorkingWithIntegers();
-            OrderPrecedence();
-            
-            int a = 7;
+             int a = 7;
             int b = 4;
             int c = 3;
             int d = (a + b) / c;
             int e = (a + b) % c;
             Console.WriteLine($"quotient: {d}");
             Console.WriteLine($"remainder: {e}");
-                    
+
+            int max = int.MaxValue;
+            int min = int.MinValue;
+            Console.WriteLine($"The range of integers is {min} to {max}");
+
+
+           int what = max + 3;
+           Console.WriteLine($"An example of overflow: {what}");  
+
+        }
+
+        static void Main(string[] args)
+        {
+            WorkingWithIntegers();
+            OrderPrecedence();
+            TestLimits();
+            
+                  
             
         }
     }
